54a0b1f742dd2c150c04161c98813c7cde5bf175
Joan <joanm95@gmail.com>
2025-04-06 18:56:22 -0400

    Change 2, Added Korra Section

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,30 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(.matchaGreen)
-                .ignoresSafeArea()
+        ScrollView{
             
-            VStack(alignment: .leading, spacing: 20) {
-                Image("Me")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(15)
-                HStack(spacing: 20) {
-                    Text("Joan")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    Text("✨ MA Insturctional Leader ✨")
+            ZStack {
+                Color(.butterYellow)
+                    .ignoresSafeArea()
+                VStack(alignment: .leading, spacing: 20) {
+                    Image("Me")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                    HStack(spacing: 20) {
+                        Text("Joan")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("✨ MA Instructional Leader ✨")
+                    }
+                    Text("It's me, ya girl!")
+                        .multilineTextAlignment(.center)
                 }
-                
-                
-                Text("Just a girl who loves matcha, her pup, and learning ASL :)")
-            }
-            .padding()
-            .background(Rectangle() .foregroundColor(.white))
+                .padding()
+                .background(Rectangle() .foregroundColor(.white))
                 .cornerRadius(15)
                 .shadow(radius: 15)
-            .padding()
+                .padding()
+            }
+            
+            ZStack {
+                Color(.brightPink)
+                    .ignoresSafeArea()
+                VStack(alignment: .leading, spacing: 20) {
+                    Image("Korra")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                    HStack(spacing: 20) {
+                        Text("Korra")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("🐾 Honorary KWK Family 🐾")
+                    }
+                    Text("It's me, ya dawg!")
+                        .multilineTextAlignment(.center)
+                }
+                .padding()
+                .background(Rectangle() .foregroundColor(.white))
+                .cornerRadius(15)
+                .shadow(radius: 15)
+                .padding()
+            }
+            
+            
+            
+            
         }
     }
 }
